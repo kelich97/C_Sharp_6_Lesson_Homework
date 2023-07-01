98b5c1e8aabfe7a2c57876533026e10e5501a090
kelich97 <135359551+kelich97@users.noreply.github.com>
2023-07-01 21:44:36 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,61 +1,26 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-//0, 7, 8, -2, -2 -> 2
-//1, -7, 567, 89, 223-> 3
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-int [] Numbers (int size)
+
+Console.WriteLine("Input b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b1 = " + b1 + " k1 = " + k1 + " b2 = " + b2 + " k2 = " + k2 );
+
+void FindPoint(double b1, double k1, double b2, double k2)
 {
-    int [] array = new int [size]; 
-    for (int i = 0; i < size; i++)
-    {
-        Console.WriteLine("Введите число");
-        array[i] = Convert.ToInt32(Console.ReadLine());     
-    }
-    return array;
+    double x = (b2 - b1) / ( k1 - k2);
+    double y = k1 * x + b1;
+    Console.Write($" -> {x} , {y}");
 }
-
-
-void Count (int [] array)
-{
-    
-    for ( int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    
-}
-
-void CountOfPositNums (int [] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > 0)
-        {
-            count++;
-        }
-    }
-    Console.Write( " -> " + count);
-}
-
-Console.WriteLine("Сколько чисел вы хотите ввести ");
-int count = Convert.ToInt32(Console.ReadLine());
-
-int [] nums = Numbers(count);
-Count(nums);
-CountOfPositNums(nums);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+FindPoint(b1, k1, b2, k2);
